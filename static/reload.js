b8a2999e8816d9beb6e8c06ee0359223432e40f4
Sebin Sequira <sebinsequira99@gmail.com>
2015-03-29 14:46:13 +0530

    bg fixed

--- static/reload.js
+++
@@ -1,7 +0,0 @@
-<script type="text/javascript">// <![CDATA[
-        function preloader(){
-            document.getElementById("loading").style.display = "none";
-            document.getElementById("content").style.display = "block";
-        }//preloader
-        window.onload = preloader;
-// ]]></script>
