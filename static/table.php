a7107705840e9d5055b445f16471cdde0d8caf74
Sarath M <sarathm00@gmail.com>
2015-04-06 17:00:06 +0530

    Added bootstrap styling to table.php

--- static/table.php
+++ static/table.php
@@ -1,31 +1,54 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
-<htmlxmlns="http://www.w3.org/1999/xhtml"> 
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="refresh" content="3">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="table.css">
+    <?php 
+    $hostname_connection = "localhost"; 
+    $database_connection = "dreadger"; 
+    $username_connection = "root"; 
+    $password_connection = "aaggss"; 
+    $connection = mysql_connect($hostname_connection, $username_connection, $password_connection) or trigger_error(mysql_error(),E_USER_ERROR); 
+    mysql_select_db($database_connection,$connection) or die( mysql_error("could not connect to database! " ) ) ; 
+    ?>
+  </head>
 
-<head> 
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
- 	<meta http-equiv="refresh" content="3">
-	<?php $hostname_connection = "localhost"; $database_connection = "dreadger"; $username_connection = "root"; $password_connection = "aaggss"; $connection = mysql_connect($hostname_connection, $username_connection, $password_connection) or trigger_error(mysql_error(),E_USER_ERROR); mysql_select_db($database_connection,$connection) or die( mysql_error("could not connect to database! " ) ) ; ?>
+  <body>
+    
+    <?php $display ="select * from dieselLevel order by mTime desc limit 1"; 
+    $result=mysql_query($display,$connection) or die(mysql_error()); 
+    if($result == FALSE) 
+      { die(mysql_error()); }
+    ?>
 
- </head>
+    <div class="container">
+                             
+      <table class="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>mTime</th>
+            <th>IP</th>
+          </tr>
+        </thead>
+        <tbody>
 
-<body> 
+          <?php while($rows = mysql_fetch_assoc($result)){ ?>
+            <tr>
+              <td><?php echo 1?></td>
+              <td><?php echo $rows['mTime'] ?></td> 
+              <td><?php echo $rows['level'] ?></td> 
+            </tr> 
+          <?php } ?>
 
-	<?php $display ="select * from dieselLevel order by mTime desc limit 1"; 
-	$result=mysql_query($display,$connection) or die(mysql_error()); 
-	if($result == FALSE) 
-		{ die(mysql_error()); }
-	?>
+        </tbody>
+      </table>
+    </div>
 
-	<table width = "245" border="0" align = "center">
-			<?php while($rows = mysql_fetch_assoc($result)){ ?>
-			<tr> <td><?php echo $rows['mTime'] ?></td> 
-			 <td><?php echo $rows['level'] ?></td> </tr> 
-			<?php } ?>
-			
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  </body>
 
-	</table>
-</body>
 </html>
-
-
-
