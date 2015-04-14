ebfca79881413b4396b7fc4d74140c5e7072b740
Sarath M <sarathm00@gmail.com>
2015-04-14 17:48:01 +0530

    tutorial file

---
+++ static/ajax/name2.php
@@ -0,0 +1,23 @@
+<?php 
+    $hostname_connection = "localhost"; 
+    $database_connection = "dreadger"; 
+    $username_connection = "admin"; 
+    $password_connection = "aaggss"; 
+    $connection = mysql_connect($hostname_connection, $username_connection, $password_connection) or trigger_error(mysql_error(),E_USER_ERROR); 
+    mysql_select_db($database_connection,$connection) or die( mysql_error("could not connect to database! " ) ) ; 
+?>
+
+<?php $display ="select * from dieselLevel order by mTime desc limit 1"; 
+    $result=mysql_query($display,$connection) or die(mysql_error()); 
+    if($result == FALSE) 
+      { die(mysql_error()); }
+?>
+
+<?php while($rows = mysql_fetch_assoc($result)){ 
+    echo 1;
+    echo 2 ;
+   	echo 3 ;
+            
+ } ?>
+
+
