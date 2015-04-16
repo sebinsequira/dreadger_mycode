06cae4511507e0265eea8daede29568ca288e1b2
Sarath M <sarathm00@gmail.com>
2015-04-16 12:05:29 +0000

    Bugs Reoved: 	1) If 'AT' in packet, then ignoring the packet 	2) Access denied for 'root'@'localhost' 	3) Logging removed

--- udpserver.py
+++ udpserver.py
@@ -45,8 +45,8 @@ class DieselLevel(Base):
 	mTime = Column(DateTime)
 	ip = Column(String(15))
 
-	def __init__(self, id,device, level, mTime, ip):
-		self.id = id
+	def __init__(self,device, level, mTime, ip):
+		#self.id = id
 		self.device = device
 		self.level = level
 		self.mTime = mTime
@@ -54,7 +54,7 @@ class DieselLevel(Base):
 
 ##Logging in as admin 
 ##creating a mysql database object object 
-engine = create_engine('mysql://root:aaggss@localhost/dreadger')
+engine = create_engine('mysql://admin:aaggss@localhost/dreadger')
 
 # packet should be or the format given below
 # "ABC123;1000;15/9/2014 13:10"
@@ -63,20 +63,22 @@ the function parameter should be a string
 return type is a (devicename,level,datetime)
 """
 def parsedata(data):
+	if 'AT' in data:
+		return (None,None,None)
 	data = data.strip()		# It removes all the newline character from the string
 	data = data.split(';')	# Splits the string at every ';' character
-	id = int(data[0])
-	device = data[1]	
-	level = int(data[2])
-	time = datetime.strptime( data[3], "%d/%m/%Y %H:%M:%S") # "21/11/06 16:30:40"
-	return (id,device, level, time)
+	#id = int(data[0])
+	device = data[0]	
+	level = int(data[1])
+	time = datetime.strptime( data[2], "%d/%m/%Y %H:%M:%S") # "21/11/06 16:30:40"
+	return (device, level, time)
 
 
 ## Main function 
 ## Here the code for opening the port is written.
 if __name__ == '__main__':	
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Creating a udp socket object 
-	HOST = '127.0.0.1'		# It is not reqiured for udp server. for udp client specify the host address 
+	HOST = ''		# It is not reqiured for udp server. for udp client specify the host address 
 	PORT = 50001	# It is port number on which communications occur 
 	try :
 		sock.bind((HOST,PORT))	# Getting the socket ready for communication at the port 50002
@@ -92,11 +94,12 @@ if __name__ == '__main__':
 	while 1: #infinite loop running to see if packets are coming to the server.
 		data, addr = sock.recvfrom(256) # Recieving 256 bits from the port.
 		try:
-			id,device, level, time = parsedata(data)
+			#id,device, level, time = parsedata(data)
+			device,level,time=parsedata(data)
 			ip,port = addr
 			s = session()
-
-			s.add(DieselLevel(id,device, level, time, ip))
+			if device != None:
+				s.add(DieselLevel(device, level, time, ip))
 			print 'data: '+ str(id),device,str(level),time,ip
 			"""
 			logger.debug ('Data packet recieved')
