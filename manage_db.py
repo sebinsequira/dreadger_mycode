6ab44918fe8f79e11a05308821af7c085e0ad84a
Sarath M <sarathm00@gmail.com>
2015-05-13 13:33:08 +0530

    Created file to manage db

--- manage_db.py
+++ manage_db.py
@@ -1,6 +1,10 @@
 from flask import Flask, flash, request, jsonify, url_for, render_template, redirect
 from flask.ext.sqlalchemy import SQLAlchemy
 from datetime import datetime
+import random
+import time
+from sqlalchemy import event
+from sqlalchemy import DDL
 
 app = Flask (__name__)
 db = SQLAlchemy(app)
@@ -8,81 +12,118 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:aaggss@localhost/dreadger
 
 
 class dreadger(db.Model):
-    __tablename__ = 'table'
+    __tablename__ = 'db'
     id                  = db.Column(db.Integer, primary_key=True)
     dreadger_name       = db.Column(db.String(25))
     time                = db.Column(db.DateTime,unique=True)  # If not unique then there will be logical errors
     storage_tank_level  = db.Column(db.Integer)
     storage_tank_cap    = db.Column(db.String(25))
-    fuel_tank_level     = db.Column(db.Integer)
-    fuel_tank_cap       = db.Column(db.String(25))
+    service_tank_level  = db.Column(db.Integer)
+    service_tank_cap    = db.Column(db.String(25))
     flowmeter_1_in      = db.Column(db.Integer)
     flowmeter_1_out     = db.Column(db.Integer)
     engine_1_status     = db.Column(db.String(25))
     flowmeter_2_in      = db.Column(db.Integer)
     flowmeter_2_out     = db.Column(db.Integer)
     engine_2_status     = db.Column(db.String(25))
-    
-    
+
+    def __repr__(self):
+        return self.dreadger_name+ ',' +str(self.time)+ ',' +str(self.storage_tank_level)+ ',' +\
+                self.storage_tank_cap+ ',' +str(self.service_tank_level)+ ',' +self.service_tank_cap+ ',' +\
+                str(self.flowmeter_1_in)+ ',' +str(self.flowmeter_1_out)+ ',' +self.engine_1_status+ ',' +str(self.flowmeter_2_in)+ ',' +\
+                str(self.flowmeter_2_out)+ ',' +str(self.engine_2_status)+'\n'
 
     def __init__(self, arg):
-    	#self.id=id
+        
         self.dreadger_name       = arg['dreadger_name']
         self.time                = arg['time']
         self.storage_tank_level  = arg['storage_tank_level']
         self.storage_tank_cap    = arg['storage_tank_cap']
-        self.fuel_tank_level     = arg['fuel_tank_level']
-        self.fuel_tank_cap       = arg['fuel_tank_cap']
+        self.service_tank_level  = arg['service_tank_level']
+        self.service_tank_cap    = arg['service_tank_cap']
         self.flowmeter_1_in      = arg['flowmeter_1_in']
         self.flowmeter_1_out     = arg['flowmeter_1_out']
         self.engine_1_status     = arg['engine_1_status']
         self.flowmeter_2_in      = arg['flowmeter_2_in']
         self.flowmeter_2_out     = arg['flowmeter_2_out']
-        engine_2_status          = arg['engine_2_status']
+        self.engine_2_status     = arg['engine_2_status']
     
 
 class database():
 
+    
     def db_init(self):
         db.create_all()
+    def drop_all(self):
+        db.drop_all()
 
     def fetchAll(self):
         try:                                                # Will fail if table doesn't exist
             data = dreadger.query.order_by(dreadger.time.desc()).all() # Select * FROM TABLE ORDER BY time
         except Exception as e:
-            flash('fetchAll: '+str(e))
-            #print 'Error:' + str(e)
-        #print data.__repr__()
-        return data
+            #flash('fetchAll: '+str(e))
+            print 'Error:' + str(e)
+        print data.__repr__()
+        
     def insertDb(self,arg):
         try:
             data=dreadger(arg)
             db.session.add(data)
             db.session.commit()
         except Exception as e:
-            flash('insertDb: '+str(e))
-            #print 'insertDb: '+str(e)
+            #flash('insertDb: '+str(e))
+            print 'insertDb: '+str(e)
         
     
     def filterRange(self,fromTime,toTime,page):
         #print "---------------------------"
         results = dreadger.query.filter(dreadger.time <= toTime).filter(dreadger.time >= fromTime).order_by(dreadger.time.desc()).paginate(page, POSTS_PER_PAGE, False)
+        
         #print results.__repr__()
         #print "---------------------------"
         return results
 
-    def dummyData(self):
+    def dummyData(self,time=datetime.now()):
+        """
+            -Inserts a single row of values into db
+            -Default value for time is datetime.now
+        """
+        storage_tank_cap = ['Open','Close']
+        service_tank_cap = ['Open','Close']
+        engine_1_status = ['ON','OFF']
+        engine_2_status = ['ON','OFF']
+
         try:
             self.db_init()
-            for i in range(1,50):
-                res = datetime.now().strftime("%Y-%m-%d %H:%M:%S")   #Converting to proper format in string
-                res = datetime.strptime(res,"%Y-%m-%d %H:%M:%S")    # Converting to proper format in datetime
-                self.insertDb('dev',i,res,'192.168.1.1')
+            arg={}
+            arg['dreadger_name']        = 'dreadger_name'
+            arg['time']                 = time
+            arg['storage_tank_level']   = random.randint(1,1000)
+            arg['storage_tank_cap']     = storage_tank_cap[random.randint(0,1)]
+            arg['service_tank_level']   = random.randint(1,1000)
+            arg['service_tank_cap']     = service_tank_cap[random.randint(0,1)]
+            arg['flowmeter_1_in']       = random.randint(1,1000) 
+            arg['flowmeter_1_out']      = random.randint(1,1000)
+            arg['engine_1_status']      = engine_1_status[random.randint(0,1)]
+            arg['flowmeter_2_in']       = random.randint(1,1000)
+            arg['flowmeter_2_out']      = random.randint(1,1000)
+            arg['engine_2_status']      = engine_2_status[random.randint(0,1)]
+            dbObj.insertDb(arg)
         except Exception as e:
-            flash('DB_init:'+str(e))
-            #print 'DB_init:'+str(e)
+            #flash('DB_init:'+str(e))
+            print 'DB_init:'+str(e)
+    
+    def dummyRange(self,num=50,fromDate='2014-01-01 00:00:00',toDate='2014-12-30 00:00:00'):
+        """
+            - Inserts a range of random values into db for time between the specified 
+                date range 
+        """
+        for x in range(0,num):
+            seed=random.random()        # for creating randomness
+            date=self.randomDate(fromDate,toDate,seed)
+            self.dummyData(date)
 
-    def randomDate(self,start, end, format, prop):
+    def randomDate(self,start, end, prop,format='%Y-%m-%d %H:%M:%S'):
         """
         Function returns a random date between start and end dates
         """
@@ -109,19 +150,8 @@ class database():
             i=i+1
 if __name__ == '__main__':
     dbObj = database()
-    #dbObj.db_init()
-    arg={}
-    arg['dreadger_name']        ='a'
-    arg['time']                 = datetime.now()
-    arg['storage_tank_level']   = random.randint()
-    arg['storage_tank_cap']     = 'b'
-    arg['fuel_tank_level']      = random.randint()
-    arg['fuel_tank_cap']        = 'c' 
-    arg['flowmeter_1_in']       = random.randint() 
-    arg['flowmeter_1_out']      = random.randint()
-    arg['engine_1_status']      = 'd'
-    arg['flowmeter_2_in']       = random.randint()
-    arg['flowmeter_2_out']      = random.randint()
-    arg['engine_2_status']      = 'e'
+    #dbObj.dummyRange()
+    #dbObj.fetchAll()
+    dbObj.filterRange('2014-08-01 00:00:00','2014-12-30 00:00:00')
 
-    dbObj.insertDb(arg)
+    
