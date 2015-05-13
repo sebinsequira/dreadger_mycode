68b0e3c12c4cb6af1291b875ffbeccfa1e5a7dbe
Sarath M <sarathm00@gmail.com>
2015-05-13 16:51:52 +0530

    Changes made for the new db

--- static/ajax/name2.php
+++ static/ajax/name2.php
@@ -7,7 +7,7 @@
     mysql_select_db($database_connection,$connection) or die( mysql_error("could not connect to database! " ) ) ; 
 ?>
 
-<?php $display ="select * from dieselLevel order by mTime desc limit 1"; 
+<?php $display ="select * from db order by time desc limit 1"; 
     $result=mysql_query($display,$connection) or die(mysql_error()); 
     if($result == FALSE) 
       { die(mysql_error()); }
@@ -15,7 +15,17 @@
 
 <?php 
     $rows = mysql_fetch_assoc($result);
-    $return_data=array('sNo'=>'1','mTime'=>$rows['mTime'],'level'=>$rows['level']);
+    $return_data=array('time' => $rows['time'],
+        'storage_tank_level'  => $rows['storage_tank_level'],
+        'storage_tank_cap'    => $rows['storage_tank_cap'],
+        'service_tank_level'  => $rows['service_tank_level'],
+        'service_tank_cap'    => $rows['service_tank_cap'],
+        'flowmeter_1_in'      => $rows['flowmeter_1_in'],
+        'flowmeter_1_out'     => $rows['flowmeter_1_out'],
+        'engine_1_status'     => $rows['engine_1_status'],
+        'flowmeter_2_in'      => $rows['flowmeter_2_in'],
+        'flowmeter_2_out'     => $rows['flowmeter_2_out'],
+        'engine_2_status'     => $rows['engine_2_status']);
     header('Content-Type: application/json');
     echo json_encode($return_data);
     exit();
