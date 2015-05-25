84067528ba7307c254a01fcbbca1dbd4845ff94c
Sarath M <sarathm00@gmail.com>
2015-05-25 15:56:07 +0530

    Updates for multiple dredgers

--- __init__.py
+++ __init__.py
@@ -19,14 +19,14 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 
 POSTS_PER_PAGE = 20  # pagination
-#app = Flask (__name__, static_url_path='\C:\Users\$$\Desktop\dreadger_bootstrap-2march20\static')
+#app = Flask (__name__, static_url_path='\C:\Users\$$\Desktop\dredger_bootstrap-2march20\static')
 app = Flask (__name__)
 
 
 
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:aaggss@localhost/dreadger'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:aaggss@localhost/dredger'
 app.secret_key = 'my secret key is this'
 login_manager = LoginManager()
 login_manager.session_protection ='strong'
@@ -62,7 +62,7 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
     
-class dieselLevel(db.Model):
+"""class dieselLevel(db.Model):
     __tablename__ = 'dieselLevel'
     id = db.Column(db.Integer, primary_key=True)
     device = db.Column(db.String(25))
@@ -77,11 +77,12 @@ class dieselLevel(db.Model):
         self.mTime = mTime
         self.ip = ip
     def __repr__(self):
-        return str(self.device)+','+str(self.level)+','+str(self.mTime)+','+str(self.ip)+'\n'
-class dreadger(db.Model):
+        return str(self.device)+','+str(self.level)+','+str(self.mTime)+','+str(self.ip)+'\n'"""
+
+class dredger(db.Model):
     __tablename__ = 'db'
     id                  = db.Column(db.Integer, primary_key=True)
-    dreadger_name       = db.Column(db.String(25))
+    dredger_name       = db.Column(db.String(25))
     time                = db.Column(db.DateTime,unique=True)  # If not unique then there will be logical errors
     storage_tank_level  = db.Column(db.Integer)
     storage_tank_cap    = db.Column(db.String(25))
@@ -95,14 +96,14 @@ class dreadger(db.Model):
     engine_2_status     = db.Column(db.String(25))
 
     def __repr__(self):
-        return self.dreadger_name+ ',' +str(self.time)+ ',' +str(self.storage_tank_level)+ ',' +\
+        return self.dredger_name+ ',' +str(self.time)+ ',' +str(self.storage_tank_level)+ ',' +\
                 self.storage_tank_cap+ ',' +str(self.service_tank_level)+ ',' +self.service_tank_cap+ ',' +\
                 str(self.flowmeter_1_in)+ ',' +str(self.flowmeter_1_out)+ ',' +self.engine_1_status+ ',' +str(self.flowmeter_2_in)+ ',' +\
                 str(self.flowmeter_2_out)+ ',' +str(self.engine_2_status)+'\n'
 
     def __init__(self, arg):
         
-        self.dreadger_name       = arg['dreadger_name']
+        self.dredger_name       = arg['dredger_name']
         self.time                = arg['time']
         self.storage_tank_level  = arg['storage_tank_level']
         self.storage_tank_cap    = arg['storage_tank_cap']
@@ -124,17 +125,17 @@ class database():
     def drop_all(self):
         db.drop_all()
 
-    def fetchAll(self):
+    def fetchAll(self,dredger_name="dredger1"):
         try:                                                # Will fail if table doesn't exist
-            data = dreadger.query.order_by(dreadger.time.desc()).all() # Select * FROM TABLE ORDER BY time
+            data = dredger.query.order_by(dredger.time.desc()).all() # Select * FROM TABLE ORDER BY time
         except Exception as e:
             flash('fetchAll: '+str(e))
             #print 'Error:' + str(e)
         #print data.__repr__()
         return data
-    def insertDb(self,arg):
+    def insertDb(self,arg,dredger_name="dredger1"):
         try:
-            data=dreadger(arg)
+            data=dredger(arg)
             db.session.add(data)
             db.session.commit()
         except Exception as e:
@@ -142,9 +143,11 @@ class database():
             #print 'insertDb: '+str(e)
         
     
-    def filterRange(self,fromTime,toTime,page):
+    def filterRange(self,fromTime,toTime,page,dredger_name="dredger1"):
         #print "---------------------------"
-        results = dreadger.query.filter(dreadger.time <= toTime).filter(dreadger.time >= fromTime).order_by(dreadger.time.desc()).paginate(page, POSTS_PER_PAGE, False)
+        results = dredger.query.filter(dredger.time <= toTime).filter(dredger.time >= fromTime).\
+                        filter_by(dredger_name = dredger_name).order_by(dredger.time.desc()).\
+                        paginate(page, POSTS_PER_PAGE, False)
         #print results.__repr__()
         #print "---------------------------"
         return results
@@ -168,7 +171,7 @@ def login():
             checkbox = False
 
         #print '--------->((('+str(userName)+')))<--------, '+ str(checkbox) + ','+ str(type(checkbox))
-        user = User.query.filter_by(email=userName).first()
+        user = User.query.filter_by(username=userName).first()
         if user is not None and user.verify_password(request.form['password']):
             login_user(user,checkbox)
             return redirect(request.args.get('next') or url_for('home'))
@@ -316,7 +319,142 @@ def home():
 
 
 
+@app.route ("/select", methods=['GET', 'POST'])
+@login_required
+def select():
+    if request.method == 'POST':
+        userName=request.form['username']
+        
+        if 'checkbox' in request.form:
+            checkbox = True
+        else:
+            checkbox = False
 
+            #print '--------->((('+str(userName)+')))<--------, '+ str(checkbox) + ','+ str(type(checkbox))
+            user = User.query.filter_by(email=userName).first()
+        if user is not None and user.verify_password(request.form['password']):
+            login_user(user,checkbox)
+            return redirect(request.args.get('next') or url_for('home'))
+        flash ('Invalid credentials!!')
+    return render_template('select.html')
+
+################# DREDGER 1 Start #############################
+@app.route ("/dredger1/home", methods=['GET', 'POST'])
+@login_required
+def dredger1_home():
+    return render_template('dredger1/home.html')
+
+@app.route ("/dredger1/filter", methods=['GET', 'POST'])
+@app.route ("/dredger1/filter/", methods=['GET', 'POST'])
+@app.route('/dredger1/filter/<int:page>', methods=['GET', 'POST'])
+
+@login_required
+def dredger1_filter(page=1,fromTime=None,toTime=None):
+
+    
+    dbObj=database()
+    if request.method == 'POST':
+        results=None
+        fromDate=request.form['fromDate']
+        fromHour=request.form['fromHour']
+        fromMin=request.form['fromMin']
+
+        toDate=request.form['toDate']
+        toHour=request.form['toHour']
+        toMin=request.form['toMin']
+
+        #print 'From:'+ str(fromDate) +','+str(fromHour)+','+str(fromMin)
+        #print 'From:'+ str(toDate) +','+str(toHour)+','+str(toMin)
+
+        fromTime= fromDate+' '+fromHour+':'+fromMin+':00'
+        toTime= toDate+' '+toHour+':'+toMin+':00'
+        
+        
+        try:
+            fromTime = datetime.strptime(fromTime, "%Y-%m-%d %H:%M:%S")
+            #fromTime = fromTime.strftime("%Y-%m-%d %H:%M:%S")
+        except ValueError as e:
+            if 'format' in str(e):
+                flash('Error in format! Invalid Entry:- "'+str(fromDate)+'".'+\
+                    '  Use "yyyy-mm-dd" format for "From Date"')
+            else:
+                flash('(From, '+str(fromDate)+'): '+str(e))
+            #print "------------>1: " + 'results= None, ' + str(len(results.items))
+            return render_template('dredger1/filter.html',results=None,fromDate=fromDate,toDate=toDate)
+        
+
+        
+        try:
+            toTime = datetime.strptime(toTime, "%Y-%m-%d %H:%M:%S")
+            #toTime = toTime.strftime("%Y-%m-%d %H:%M:%S")
+        except ValueError as e:
+            if 'format' in str(e):
+                flash('Error in format! Invalid Entry:- "'+str(toDate)+'".'+\
+                    '  Use "yyyy-mm-dd" format for "To Date"')
+            else:
+                flash('(To, '+str(toDate)+'): '+str(e))
+            #print "------------>2: " + 'results= None, ' + str(len(results.items))
+            return render_template('dredger1/filter.html',results=None,fromDate=fromDate,toDate=toDate)
+        
+        #print 'from:'+str(type(fromTime))+': '+str(fromTime)
+        #print 'to:'+str(type(toTime))+': '+str(toTime)
+
+        #results=dbObj.filterRange(fromTime,toTime,page)
+        """fromTime='2008-02-16 00:00:00'
+        toTime='2015-04-12 00:00:00'"""
+        #1
+        
+
+        
+        results = dbObj.filterRange(fromTime,toTime,1,"dredger2")
+        #print 'fromTime='+str(fromTime)
+        #print 'toTime='+str(toTime)
+        #print 'results='+str(results)
+        #print 'request.method='+str(request.method)
+        
+        if not results:
+            results=None
+
+
+        
+        try:
+            fromDate
+            toDate
+        except NameError:
+            #print "------------>3: " + 'results= ' + str(len(results.items))
+            return render_template('dredger1/filter.html',results=results,fromTime=fromTime,toTime=toTime)        # If fromDate and toDate doesn't exist, then the page is being loaded for the first time                          
+            #return "Hello"
+        else:
+            #print "------------>4: " + 'results= ' + str(len(results.items))
+            return render_template('dredger1/filter.html',results=results,fromDate=fromDate,toDate=toDate,fromTime=fromTime,toTime=toTime)  # To make sure the date and time data doesn't vanish when clicking accept
+            #return render_template('dredger1/filter.html',results=results)
+            #return "Hello World"
+        
+    fromTime=request.args.get('fromTime','')
+    toTime=request.args.get('toTime','')
+    #2
+    #print '-----------------------------------------------------------'
+    
+    if fromTime and toTime:
+        results = dbObj.filterRange(fromTime,toTime,page,"dredger2")
+    else:
+        results=None
+    
+        
+    """print "------------>5: " + 'page= '+str(page)+'results= ' ,
+                if results:
+                    str(len(results.items))
+                else:
+                    print 'None'"""
+
+    #print 'fromTime='+str(fromTime)
+    #print 'toTime='+str(toTime)
+    #print 'results='+str(results)
+    #print 'request.method='+str(request.method)
+    #print '-----------------------------------------------------------'
+    return render_template('dredger1/filter.html',results=results,fromTime=fromTime,toTime=toTime)
+
+################# DREDGER 1 End #############################
 @app.route("/logout",methods=["GET"])
 @login_required
 def logout():
