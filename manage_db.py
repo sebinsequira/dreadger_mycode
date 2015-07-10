95cf2e648b47cb6d84a526c02d87de5f290983b6
Sarath M <sarathm00@gmail.com>
2015-07-10 18:26:42 +0530

    Removed UNIQUE constraint for time column

--- manage_db.py
+++ manage_db.py
@@ -21,7 +21,6 @@ def table_create():
      error_main VARCHAR(20),\
      error_timeout VARCHAR(20),\
      error_unknown VARCHAR(20),\
-     PRIMARY KEY (id),'\
-     )
+     PRIMARY KEY (id)')
 
 table_create()
